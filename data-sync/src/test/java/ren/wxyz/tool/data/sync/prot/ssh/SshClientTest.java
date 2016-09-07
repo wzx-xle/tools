a3c830382183bf78acca88c4f73a1b1f31504af5
wxyz <hyhjwzx@126.com>
2016-09-07 19:50:34 +0800

    ssh客户端的文件列表功能实现，并测试

---
+++ data-sync/src/test/java/ren/wxyz/tool/data/sync/prot/ssh/SshClientTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2001-2016 wxyz <hyhjwzx@126.com>
+ * <p/>
+ * This program can be distributed under the terms of the GNU GPL Version 2.
+ * See the file LICENSE.
+ */
+package ren.wxyz.tool.data.sync.prot.ssh;
+
+import org.junit.*;
+import ren.wxyz.tool.data.sync.prot.FileInfo;
+
+import java.util.List;
+
+/**
+ * 测试SSH客户端
+ *
+ * @auther wxyz 2016-09-07_16:20
+ * @since 1.0
+ */
+@Ignore
+public class SshClientTest {
+
+    private static SshClient ssh;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ssh = new SshClient("172.16.10.40", 22, "root", "iflytek!40");
+        ssh.setWorkDirectory(".");
+        ssh.openSession();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        ssh.closeSession();
+        ssh = null;
+    }
+
+    @Test
+    public void testList() throws Exception {
+        String workDir = "/root/data-service";
+
+        String path = "/root";
+        List<FileInfo> files = ssh.list(path, true);
+        System.out.println(files.size());
+        for (FileInfo file : files) {
+            System.out.println(file.getAbsolutePath());
+        }
+
+        path = "/root/uc.zip";
+        files = ssh.list(path, true);
+        System.out.println(files.size());
+        for (FileInfo file : files) {
+            System.out.println(file.getAbsolutePath());
+        }
+
+        ssh.setWorkDirectory(workDir);
+        path = "dataexchange";
+        files = ssh.list(path, true);
+        System.out.println(files.size());
+        for (FileInfo file : files) {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+}
