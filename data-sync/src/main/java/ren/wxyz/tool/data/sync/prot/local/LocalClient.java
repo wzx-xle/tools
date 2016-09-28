7dafe810c556bf34726602fda88d9e7fbb2e5080
wxyz <hyhjwzx@126.com>
2016-09-28 18:18:04 +0800

    data-sync: 本地客户端完成遍历文件列表的功能

---
+++ data-sync/src/main/java/ren/wxyz/tool/data/sync/prot/local/LocalClient.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (C) 2001-2016 wxyz <hyhjwzx@126.com>
+ * <p/>
+ * This program can be distributed under the terms of the GNU GPL Version 2.
+ * See the file LICENSE.
+ */
+package ren.wxyz.tool.data.sync.prot.local;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import ren.wxyz.tool.common.file.PathHelper;
+import ren.wxyz.tool.data.sync.prot.FileInfo;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 访问本地文件
+ *
+ * @auther wxyz 2016-09-08_09:40
+ * @since 1.0
+ */
+@Slf4j
+@Setter
+public class LocalClient {
+
+    /**
+     * 默认目录
+     */
+    private String workDir;
+
+    /**
+     * 构造客户端事，设置工作目录
+     *
+     * @param workDir 工作目录
+     */
+    public LocalClient(String workDir) {
+        setWorkDir(workDir);
+    }
+
+    /**
+     * 获取路径下的文件列表
+     *
+     * @param path 要列表的路径
+     * @param subDir 是否处理子目录
+     * @return 文件列表
+     */
+    public List<FileInfo> list(String path, boolean subDir) {
+        List<FileInfo> files = list(path);
+
+        if (subDir) {
+            int idx = 0;
+            while (idx < files.size()) {
+                FileInfo file = files.get(idx++);
+                if (file.getFileType() == FileInfo.Type.DIR) {
+                    List<FileInfo> tmp = list(file.getAbsolutePath());
+                    files.addAll(tmp);
+                }
+            }
+        }
+
+        return files;
+    }
+
+    /**
+     * 获取路径下的文件列表，不包括子目录
+     *
+     * @param path 要列表的路径
+     * @return 文件列表
+     */
+    private List<FileInfo> list(String path) {
+        List<FileInfo> files = new ArrayList<>();
+        File p = new File(PathHelper.isAbsolute(path) ? path : PathHelper.join("\\", this.workDir, path));
+
+        // 遍历目录下的文件
+        File[] filelist = p.listFiles();
+        for (File f : filelist) {
+            FileInfo fi = new FileInfo();
+            fi.setAbsolutePath(f.getAbsolutePath());
+            fi.setFilename(f.getName());
+            fi.setFileSize(f.length());
+            fi.setModifyDate(new Date(f.lastModified()));
+            fi.setFileType(getFileType(f));
+
+            files.add(fi);
+        }
+
+        return files;
+    }
+
+    /**
+     * 获取文件类型
+     *
+     * @param f 文件对象
+     * @return 文件类型
+     */
+    private FileInfo.Type getFileType(File f) {
+        if (f.isFile()) {
+            return FileInfo.Type.REG;
+        }
+
+        if (f.isDirectory()) {
+            return FileInfo.Type.DIR;
+        }
+
+        throw new RuntimeException("找不到文件类型");
+    }
+}
---
+++ data-sync/src/test/java/ren/wxyz/tool/data/sync/prot/local/LocalClientTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2001-2016 wxyz <hyhjwzx@126.com>
+ * <p/>
+ * This program can be distributed under the terms of the GNU GPL Version 2.
+ * See the file LICENSE.
+ */
+package ren.wxyz.tool.data.sync.prot.local;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ren.wxyz.tool.common.file.PathHelper;
+import ren.wxyz.tool.data.sync.prot.FileInfo;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * 测试本地客户端
+ *
+ * @auther wxyz 2016-09-27_11:49
+ * @since 1.0
+ */
+public class LocalClientTest {
+
+    private static LocalClient localClient;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        localClient = new LocalClient(PathHelper.getClassPath(LocalClientTest.class));
+    }
+
+    @Test
+    public void testList() throws Exception {
+        String path = "test-local-client";
+
+        List<FileInfo> files = localClient.list(path, true);
+        printFileInfo(files);
+
+        files = localClient.list(path, false);
+        printFileInfo(files);
+
+        
+    }
+
+    private void printFileInfo(List<FileInfo> files) {
+        String printFormat = "%s,%d,%d";
+        System.out.println(files.size());
+        for (FileInfo f : files) {
+            System.out.println(String.format(printFormat,
+                    f.getAbsolutePath(), f.getFileSize(), f.getModifyDate().getTime()));
+        }
+    }
+}
