00f9c017be96049e1e777d6debeb59f7e04a829e
wxyz <hyhjwzx@126.com>
2016-09-30 21:22:32 +0800

    data-sync: 文件同步业务类

---
+++ data-sync/src/main/java/ren/wxyz/tool/data/sync/FileSync.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2001-2016 wxyz <hyhjwzx@126.com>
+ * <p/>
+ * This program can be distributed under the terms of the GNU GPL Version 2.
+ * See the file LICENSE.
+ */
+package ren.wxyz.tool.data.sync;
+
+/**
+ * 文件同步
+ *
+ * @auther wxyz 2016-09-30_15:26
+ * @since 1.0
+ */
+public class FileSync {
+}
